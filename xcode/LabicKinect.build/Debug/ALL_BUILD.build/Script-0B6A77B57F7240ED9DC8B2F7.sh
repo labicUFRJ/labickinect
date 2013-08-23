e34dadd80f52fde8a48d9a5a749f6273f6b51573
Mario Cecchi <macecchi@gmail.com>
2013-08-23 01:37:36 -0300

    Excluindo binarios e lixos2

--- xcode/LabicKinect.build/Debug/ALL_BUILD.build/Script-0B6A77B57F7240ED9DC8B2F7.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-make -C /Users/macecchi/Desktop/labickinect/xcode -f /Users/macecchi/Desktop/labickinect/xcode/CMakeScripts/ALL_BUILD_cmakeRulesBuildPhase.make$CONFIGURATION all
--- xcode/LabicKinect.build/Debug/ZERO_CHECK.build/Script-C44D56F592B24DFAA1F90CC4.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-make -C /Users/macecchi/Desktop/labickinect/xcode -f /Users/macecchi/Desktop/labickinect/xcode/CMakeScripts/ZERO_CHECK_cmakeRulesBuildPhase.make$CONFIGURATION all
