e272557f57638576673b9fe1edad42cb7b85d92d
Mario Cecchi <macecchi@gmail.com>
2013-08-23 01:37:36 -0300

    Excluindo binarios e lixos2

--- xcode/LabicKinect.build/Debug/labickinect.build/Objects-normal/x86_64/LabicKinect.d
+++
@@ -1,5 +0,0 @@
-dependencies: /Users/macecchi/Desktop/labickinect/LabicKinect.cpp \
-  /Users/macecchi/Desktop/labickinect/LabicKinect.h \
-  /Users/macecchi/Desktop/labickinect/libfreenect.h \
-  /usr/local/include/libfreenect/libfreenect.h \
-  /opt/local/include/libusb-1.0/libusb.h
--- xcode/LabicKinect.build/Debug/labickinect.build/Script-38DF38A4599E40FA83B111E5.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-make -C /Users/macecchi/Desktop/labickinect/xcode -f /Users/macecchi/Desktop/labickinect/xcode/CMakeScripts/labickinect_postBuildPhase.make$CONFIGURATION all
